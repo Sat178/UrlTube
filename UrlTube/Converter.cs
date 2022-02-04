cb132389c5998cc908a0384dbaf001d7ce81e705
Avoidy <95578426+Avoidy@users.noreply.github.com>
2022-02-04 15:40:42 +0100

    Add project files.

---
+++ UrlTube/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UrlTube
+{
+    internal class Converter
+    {
+        public static void Loader()
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Open Video Urls";
+                ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                ofd.FilterIndex = 2;
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string fPath = ofd.FileName;
+                    var fUrls = File.ReadAllLines(fPath);
+                    foreach (string fUrl in fUrls) Program.rawUrls.Add(fUrl);
+                    foreach (string fUrl in Program.rawUrls) Program.FilteredUrls.Add(videoID(fUrl));
+                    Program.FilteredUrls.Distinct<string>();
+                    foreach (string fUrl in Program.FilteredUrls) Console.WriteLine($"{fUrl}");
+
+                }
+            }
+
+            
+        }
+        public static string videoID(string vidUrl)
+        {
+            if (vidUrl.Contains("https://www.youtube.com/watch?v="))
+            {
+                vidUrl = vidUrl.Split(new string[]
+                {
+                        "https://www.youtube.com/watch?v="
+
+                }, StringSplitOptions.None)[1];
+            }
+
+            return vidUrl;
+        }
+        public static void Trimmer()
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UrlTube/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlTube
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args) { 
+
+            Converter.Loader();
+            Console.ReadKey();
+        }
+
+        public static List<string> rawUrls = new List<string>();
+        public static List<string> FilteredUrls = new List<string>();
+    }
+}
