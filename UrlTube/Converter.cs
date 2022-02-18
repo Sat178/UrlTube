a2590925d4a2b4f8e598f76cfb3ed521ebc27685
Avoidy <95578426+Avoidy@users.noreply.github.com>
2022-02-18 21:02:09 +0100

    fix: duplicates, dir

--- UrlTube/Converter.cs
+++ UrlTube/Converter.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
-using System.Windows.Forms;
-using System.IO;
-using System.Net;
-using Leaf.xNet;
+﻿using Leaf.xNet;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+
 
 namespace UrlTube
 {
@@ -17,36 +13,35 @@ namespace UrlTube
     {
         public static void Loader()
         {
-            using (OpenFileDialog ofd = new OpenFileDialog())
+            using (OpenFileDialog ofd = new OpenFileDialog()) // Directory to open your URL file
             {
                 ofd.Title = "Open Video Urls";
                 ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
                 ofd.FilterIndex = 2;
                 ofd.RestoreDirectory = true;
 
-                if (ofd.ShowDialog() == DialogResult.OK)
+                if (ofd.ShowDialog() == DialogResult.OK) // If file is valid go-on!
                 {
                     string fPath = ofd.FileName;
-                    var fUrls = File.ReadAllLines(fPath);
-                    foreach (string fUrl in fUrls) Program.rawUrls.Add(fUrl);
-                    foreach (string fUrl in Program.rawUrls) Program.FilteredUrls.Add(videoID(fUrl));
-                    Program.FilteredUrls = Program.FilteredUrls.Distinct().ToList<string>();
-                    foreach (string fID in Program.FilteredUrls) Program.UrlsDL.Add(videoDL(fID));
-                    //foreach (string finalDL in Program.UrlsDL) Console.WriteLine($"{finalDL}");
+                    var fUrls = File.ReadAllLines(fPath); // Read all lines/urls in the .txt file
+                    foreach (string fUrl in fUrls) Program.rawUrls.Add(fUrl); // Add all urls into List<string>
+                    foreach (string fUrl in Program.rawUrls) Program.FilteredUrls.Add(videoID(fUrl)); //Foreach url cycle thru videoID to get VideoID (which is currently off, because I don't need it for now)
+                    Program.FilteredUrls = Program.FilteredUrls.Distinct().ToList<string>(); // Distinct (remove duplicates from lists)
+                    foreach (string fID in Program.FilteredUrls) Program.UrlsDL.Add(videoDL(fID)); // Add all POST-ready request into List<string>
+                                                                                                   //foreach (string finalDL in Program.UrlsDL) Console.WriteLine($"{finalDL}");
                     foreach (string dlLoc in Program.UrlsDL) Downloader(dlLoc);
 
                 }
-            }     
+            }
         }
         public static string videoID(string vidUrl)
         {
             if (vidUrl.Contains("https://www.youtube.com/watch?v="))
             {
-                vidUrl = vidUrl.Split(new string[]
-                {
-                        " ",
+                vidUrl = vidUrl.Split(new string[] {
+          " ", // Remove unnecessary blank spaces {"https://www.youtube.com/watch?v="}
 
-                }, StringSplitOptions.None)[0];
+        }, StringSplitOptions.None)[0];
             }
 
             return vidUrl;
@@ -54,15 +49,15 @@ namespace UrlTube
 
         public static string videoDL(string vidID)
         {
-            string Ldl = "{\"url\":\"";
-            string Ldr = "\"}";
+            string Ldl = "{\"url\":\""; //Necessary for POST request
+            string Ldr = "\"}"; //Necessary for POST request
 
-            return string.Format("{0}{1}{2}", new object[]
-            {
-                Ldl,
-                vidID,
-                Ldr
-            });
+            return string.Format("{0}{1}{2}", new object[] // Format string into right POST-format
+              {
+          Ldl,
+          vidID,
+          Ldr
+              });
         }
 
         public static void Downloader(string dlLoc)
@@ -75,66 +70,117 @@ namespace UrlTube
                 hr.IgnoreInvalidCookie = false;
                 hr.IgnoreProtocolErrors = true;
 
+                string hID = dlLoc.Split(new string[] // Example {"url":"https://www.youtube.com/watch?v=t7a-b1zTS60"}
+                  {
+            "{\"url\":\"https://www.youtube.com/watch?v=",
+                  }, StringSplitOptions.None)[1];
+
+                Program.fullID = hID.Split(new string[] {
+          "\"}"
+        }, StringSplitOptions.None)[0];
+
+                //Console.WriteLine(fullID);
+
                 var rwData = hr.Post("https://api.onlinevideoconverter.pro/api/convert", dlLoc, "application/json").ToString();
                 JsonParser(rwData);
-                Console.WriteLine(rwData);
+                //Console.WriteLine(rwData);
+                Console.WriteLine("Finished: {0}", Program.fullID);
             }
-                
-            /*
-            var x = webBrowser.Document.GetElementById("buttonTitle").ToString();
-            Console.WriteLine(x);
-
-            webBrowser.Document.GetElementById("downloadButton").InvokeMember("onclick");
-
-            */
-
-            // Console.WriteLine(pInfo);
 
         }
 
         public static void JsonParser(string rawData)
         {
+
             try
             {
                 JObject jobj = JObject.Parse(rawData);
 
                 string Urls = jobj.SelectToken("url").ToString();
                 //Console.WriteLine(Urls);
-
-                string dlUrl = Urls.Split(new string[]
+                try
                 {
-                    "https://du."
-                }, StringSplitOptions.None)[1];
+                    if (Urls.Contains("https://du."))
+                    {
 
-                //Console.WriteLine(dlUrl);
+                        string dlUrl = Urls.Split(new string[] {
+              "https://du."
+            }, StringSplitOptions.None)[1];
 
-                string fdlUrl = dlUrl.Split(new string[]
+                        //Console.WriteLine(dlUrl);
+
+                        string fdlUrl = dlUrl.Split(new string[] {
+              "\","
+            }, StringSplitOptions.None)[0];
+
+                        string append = "https://du.";
+
+                        Program.rdyUrl = string.Format("{0}{1}", new object[] {
+              append,
+              fdlUrl
+            });
+                    }
+                    else
+                    {
+                        string dlUrl = Urls.Split(new string[] {
+              "https:\\/\\/ "
+            }, StringSplitOptions.None)[1];
+
+                        string fdlUrl = dlUrl.Split(new string[] {
+              "\","
+            }, StringSplitOptions.None)[0];
+
+                        string append = "https:\\/\\/";
+
+                        Program.rdyUrl = string.Format("{0}{1}", new object[] {
+              append,
+              fdlUrl
+            });
+                    }
+
+                }
+                catch
                 {
-                    "\","
-                }, StringSplitOptions.None)[0];
 
-                string append = "https://du.";
+                }
 
-                string rdyUrl = string.Format("{0}{1}", new object[]
-                {
-                    append,
-                    fdlUrl
-                });
+                dirDownload(Program.rdyUrl);
+                //Console.WriteLine("Finished: {0}", Program.fullID);
 
-                Console.WriteLine(rdyUrl);
-                
+                //Console.WriteLine(rdyUrl);
 
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
-                //Console.WriteLine(e);
+                Console.WriteLine(e);
+                Console.WriteLine("Failed: {0}", Program.fullID);
+                //Console.WriteLine(rawData);
+                return;
+            }
+        }
+
+        public static void dirDownload(string downloadUrl)
+        {
+            using (var Client = new WebClient())
+            {
+                string dlloc = AppDomain.CurrentDomain.BaseDirectory + "\\" + Program.fullID + ".mp4";
+                Client.DownloadFile(downloadUrl, dlloc);
                 return;
             }
         }
 
         public class vidInfo
         {
-            public string stream { get; set; }
-            public string url { get; set; }
+            public string stream
+            {
+                get;
+                set;
+            }
+            public string url
+            {
+                get;
+                set;
+            }
         }
         public static void Trimmer()
         {
--- UrlTube/Program.cs
+++ UrlTube/Program.cs
@@ -8,16 +8,18 @@ namespace UrlTube
 {
     internal class Program
     {
-        [STAThread]
+        [STAThread] // Required to open Directory
         static void Main(string[] args) { 
 
             Converter.Loader();
-            Console.ReadKey();
+            Console.ReadKey(); // Prevent closing program
         }
 
         public static List<string> rawUrls = new List<string>();
         public static List<string> FilteredUrls = new List<string>();
         public static List<string> UrlsDL = new List<string>();
         public static List<string> objs = new List<string>();
+        public static string fullID = string.Empty;
+        public static string rdyUrl = string.Empty;
     }
 }
